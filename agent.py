29dc85863b45274c99664831f987ba2633f356da
Francois Kruger <francoiskrugermail@gmail.com>
2026-03-23 20:21:28 -0400

    Updated the agent to a point where it is building a basic Q table and saving it out

--- agent.py
+++ agent.py
@@ -1,28 +1,33 @@
 import json
 from datetime import datetime
+import os
 
 class Agent:
     quality_table = {}
 
     def export_agent(self):
-        agent_path = f"agent_{datetime.now().strftime('%Y%m%d%H%M%S')}.json"
-        with open(agent_path, 'wb') as file:
-            json.dump(self, f)
+        agents_dir = "./agent_runs"
+        os.makedirs(agents_dir, exist_ok=True)
+        filename = f"agent_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.json"
+        agent_path = f"./{agents_dir}/{filename}"
+        with open(agent_path, 'w') as file:
+            json.dump(self.quality_table, file, indent=4)
 
     def import_agent(self, agent_path: str):
-        with open(agent_path, 'rb') as file:
-            self.quality_table = json.load(f)
+        with open(agent_path, 'r') as file:
+            self.quality_table = json.load(file)
 
     def learn(self, state):
         state_hash = self.get_state_hash(state)
         if state_hash not in self.quality_table:
             self.quality_table[state_hash] = [0, 0]
-        self.quality_table[state_hash] += 1
+        # self.quality_table[state_hash] = [0, 1]
     
     def get_state_hash(self, state):
-        return (
-            state.bird_y,
-            state.bird_x,
-            state.next_pipe_gap_center_y,
-            state.next_pipe_distance_x,
+        tuple_data = (
+            int(state.bird_y),
+            int(state.bird_x),
+            int(state.next_pipe_gap_center_y or 0),
+            int(state.next_pipe_distance_x or 0),
         )
+        return str(tuple_data)
--- run.py
+++ run.py
@@ -4,25 +4,29 @@ import random
 import time
 
 from game.main import Game
+from agent import Agent
 
-# Pause between steps so you can follow the bird and pipes (seconds).
 FRAME_DELAY_SECONDS = 1.0 / 30
-
-# Each loop iteration: independent probability of flapping (0.0 = never, 1.0 = always).
 RANDOM_FLAP_PROBABILITY = 0.01
 
 
 def main() -> None:
     game = Game()
+    agent = Agent()
     state = game.next_action(flap=True)  # start + first flap
+    agent.learn(state)
+
     time.sleep(FRAME_DELAY_SECONDS)
-    for _ in range(200):
+    for _ in range(100):
         if state.game_state != "playing":
             should_flap = True
         else:
             should_flap = random.random() < RANDOM_FLAP_PROBABILITY
         state = game.next_action(flap=should_flap)
+        agent.learn(state)
         time.sleep(FRAME_DELAY_SECONDS)
+
+    agent.export_agent()
 
 
 if __name__ == "__main__":
